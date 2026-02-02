15271a4d7f9083b684fc5d9926dbf23c0a781d9a
Rishikesh1411 <rishikeshr335@gmail.com>
2026-02-02 23:31:19 +0530

    change commited

--- 7_Advance_pattern_problem/problem.cpp
+++ 7_Advance_pattern_problem/problem.cpp
@@ -30,14 +30,44 @@ int main(){
     //     cout<<endl;
     // }
 
-    //4.print numver in reverse pattern
-    int row,col;
-    for(row=1;row<=5;row++){
-        for(col=row;col>=1;col--){
-            cout<<col<<" ";
-        }
-        cout<<endl;
-    }
+    // //4.print numver in reverse pattern
+    // int row,col;
+    // for(row=1;row<=5;row++){
+    //     for(col=row;col>=1;col--){
+    //         cout<<col<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    // 5.print alphabet pattern
+    // int row,col;
+   
+    // for(row=1;row<=5;row++){
+    //     char ch = ('A'-1)+row;
+    //     for(col=1;col<=row;col++){
+    //         cout<<ch<<" ";
+            
+    //     }
+    //     cout<<endl;
+    // }
+
+    // //method 2
+    // char row,col;
+    // for(row='a';row<='e';row++){
+    //     for(col='a';col<=row;col++){
+    //         cout<<row<<" ";
+    //     }
+    //     cout<<endl;
+    // }
+
+    //6.print * in reverse order
+    // int row,col;
+    // for(row=5;row>=1;row--){
+    //     for(col=1;col<=row;col++){
+    //         cout<<"* ";
+    //     }
+    //     cout<<endl;
+    // }
 
 
 
