2c9077fd11dcf3e144f47c74a5fe69ff7f836faf
Rishikesh1411 <rishikeshr335@gmail.com>
2026-03-30 15:51:52 +0530

    files added

---
+++ day_10_operators/assignment.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+// int main() {
+//     1.assignment operator example
+//     int a = 5; // assigns the value 5 to variable a
+//     cout << "Value of a: " << a << endl; // Output: 5
+//     return 0;
+
+//     2.increment
+//     int a = 5;
+//     cout << "Value of a before increment: " << a << endl; // Output:
+//     a++; // post-increment: a becomes 6 after this line
+//     cout << "Value of a after post-increment: " << a << endl;
+//     ++a; // pre-increment: a becomes 7 before this line
+//     cout << "Value of a after pre-increment: " << a << endl;
+//     return 0;
+
+//
+    //3.decrement
+    // int a = 5;
+    // cout << "Value of a before decrement: " << a << endl; // Output: 5
+    // a--; // post-decrement: a becomes 4 after this line
+    // cout << "Value of a after post-decrement: " << a << endl;
+    // --a; // pre-decrement: a becomes 3 before this line
+    // cout << "Value of a after pre-decrement: " << a << endl;
+    // return 0;
+//}
+
+
---
+++ day_10_operators/bitwise.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+//    // bitwise(&) example
+//     int a = 5, b = 10;
+//     int result = a & b; // 5 (0101) & 10 (1010) = 0 (0000)
+//     cout << "Result of a & b: " << result << endl; // Output: 0
+//     return 0;
+
+    // bitwise(|) example
+    // int a = 5, b = 10;
+    // int result = a | b; // 5 (0101) | 10 (1010) = 15 (1111)
+    // cout << "Result of a | b: " << result << endl; // Output: 15
+    // return 0;
+
+    // bitwise(^) example
+    // int a = 5, b = 10;
+    // int result = a ^ b; // 5 (0101) ^ 10 (1010) = 15 (1111)
+    // cout << "Result of a ^ b: " << result << endl; // Output: 15
+    // return 0;
+
+    // // bitwise(~) example
+    // int a = 5;
+    // int result = ~a; // ~5 (0101) = -6 (1010 in two's complement)
+    // cout << "Result of ~a: " << result << endl; // Output: -6
+    // return 0;
+
+    // bitwise left shift (<<) example`
+    // int a = 5; // 5 (0000 0101)
+    // int result = a << 2; // 5 (0000 010
+    // cout << "Result of a << 2: " << result << endl; // Output: 20
+    // return 0;
+
+    // bitwise right shift (>>) example
+    int a = 20; // 20 (0001 0100)
+    int result = a >> 2; // 20 (0001 0100) >> 2 = 5 (0000 0101)
+    cout << "Result of a >> 2: " << result << endl; // Output
+    return 0;
+}
+
+
