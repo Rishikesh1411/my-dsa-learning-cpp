25950d783e60c28e56ad9c20050ec86a530dab27
Rishikesh1411 <rishikeshr335@gmail.com>
2026-03-30 15:27:50 +0530

    files added

---
+++ day_10_operators/logical.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+// int main() {
+//    1. Logical AND (&&) example
+//     int a = 5, b = 10;
+//     bool result = (a < b) && (b > a);
+//     cout << "Result of (a < b) && (b > a): " << result << endl; // Output: 1 (true)
+//     return 0;
+
+    // 2. Logical OR (||) example
+    // int a = 5, b = 10;
+    // bool result = (a > b) || (b > a);
+    // cout << "Result of (a > b) || (b > a): " << result << endl; // Output: 1 (true)
+    // return 0;
+
+    //3. Logical NOT (!) example
+    // int a = 5, b = 10;
+    // bool result = !(a > b);
+    // cout << "Result of !(a > b): " << result << endl; // Output: 1 (true)
+    // return 0;
+      
+// }
