8d2e6c49a29d40a385d7b7fb7bc40cd48b733b2d
Rishikesh1411 <rishikeshr335@gmail.com>
2026-03-30 15:23:58 +0530

    files added

---
+++ day_10_operators/compareason.cpp
@@ -0,0 +1,101 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    // // 1. a==b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+
+    // if (a==b){
+    //     cout<<"a is equal to b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not equal to b"<<endl;
+    // }
+
+    //2. a!=b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+
+    // if (a!=b){
+    //     cout<<"a is not equal to b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is equal to b"<<endl;
+    // }
+    
+    //3. a>b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+
+    // if (a>b){
+    //     cout<<"a is greater than b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not greater than b"<<endl;
+    // }
+
+    //4. a<b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+
+    // if (a<b){
+    //     cout<<"a is less than b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not less than b"<<endl;
+    // }
+
+    //5. a>=b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+
+    // if (a>=b){
+    //     cout<<"a is greater than or equal to b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not greater than or equal to b"<<endl;
+    // }
+
+    //6. a<=b example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+    
+    // if (a<=b){
+    //     cout<<"a is less than or equal to b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not less than or equal to b"<<endl;
+
+    //precision order example
+    // int a,b;
+    // cout<<"Enter your first numbers: ";
+    // cin>>a ;
+    // cout<<"Enter yout Second number: ";
+    // cin>>b ;
+    
+    // if (a<=b){
+    //     cout<<"a is less than or equal to b"<<endl;
+    // }
+    // else {
+    //     cout<<"a is not less than or equal to b"<<endl;
+
+    // }
