a9c4ec5a187fb895f47ff36e41dbe159214cb63d
Rishikesh1411 <rishikeshr335@gmail.com>
2026-04-08 13:28:31 +0530

    files added

---
+++ day_14_Function/function.cpp
@@ -0,0 +1,76 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+// Function declaration
+
+
+// //1.prime number function
+// bool isPrime(int num)
+// {
+//     if (num <= 1) return false; // 0 and 1 are not prime numbers
+//     for (int i = 2; i <= num / 2; i++) {
+//         if (num % i == 0) return false; // If num is divisible by any number other than 1 and itself, it's not prime
+//     }
+//     return true; // If num is greater than 1 and not divisible by any number other than 1 and itself, it's prime
+// }
+
+// int main()
+// {
+//     int num;
+//     cout << "Enter a number: ";
+//     cin >> num;
+//     if (isPrime(num)) {  //Num is here a argument for the function isPrime
+//         cout << num << " is a prime number." << endl;
+//     } else {
+//         cout << num << " is not a prime number." << endl;
+//     }
+//     return 0;
+
+
+// }
+
+// //2.sum function
+
+// int addSum(int a,int b)
+// {
+//     return a+b;
+// }
+
+// int main()
+// {
+//     int num1,num2;
+//     cout << "Enter two numbers: ";
+//     cin >> num1 >> num2;
+//     int sum = addSum(num1,num2);
+//     cout << "Sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+//     return 0;
+
+// }
+
+// //3.multiplication function
+// int multiply()
+// {
+//     int num1,num2;
+//     cout << "Enter two numbers: ";
+//     cin >> num1 >> num2;
+//     return num1*num2;
+// }
+
+// int main()
+// {
+//     int result = multiply();
+//     cout << "The product of the two numbers is: " << result << endl;
+// }
+
+// //4.void function
+// void greet()
+// {
+//     cout << "Hello! Welcome to C++ programming." << endl;
+// }
+
+// int main()
+// {
+//     greet();
+//     return 0;
+// }
